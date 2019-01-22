78398cb6a525b583c87fa9146570ac3f730526a7
Marcus Perlick <mperlick@fractalqb.de>
2019-01-22 19:24:44 +0100

    .

--- qbsllm.go
+++ qbsllm.go
@@ -1,42 +1,116 @@
 package qbsllm
 
 import (
-	"context"
 	"io"
 
 	"git.fractalqb.de/fractalqb/qblog"
 	"git.fractalqb.de/fractalqb/sllm"
 )
 
-type Logger qblog.Logger
+type Logger struct {
+	qblog.Logger
+}
 
 func New(level qblog.Level, title string, wr io.Writer, fmt qblog.Formatter) *Logger {
-	res := qblog.New(level, title, wr, fmt)
-	return (*Logger)(res)
+	tmp := qblog.New(level, title, wr, fmt)
+	res := &Logger{*tmp}
+	return res
 }
 
 const calldepth = 3
 
-func (l *Logger) CtxWr(level qblog.Level, ctx context.Context, message string) {
-	(*qblog.Logger)(l).Out(calldepth, level, ctx, qblog.Msg(message))
-}
-
-func (l *Logger) Wr(level qblog.Level, message string) {
-	(*qblog.Logger)(l).Out(calldepth, level, nil, qblog.Msg(message))
-}
-
-func (l *Logger) CtxArgs(level qblog.Level, ctx context.Context, tmpl string, args ...interface{}) {
-	(*qblog.Logger)(l).Out(calldepth, level, ctx, sllm.Args(tmpl, args...))
+func (l *Logger) Msg(level qblog.Level, message string) {
+	l.Out(calldepth, level, qblog.Msg(message))
 }
 
 func (l *Logger) Args(level qblog.Level, tmpl string, args ...interface{}) {
-	(*qblog.Logger)(l).Out(calldepth, level, nil, sllm.Args(tmpl, args...))
-}
-
-func (l *Logger) CtxMap(level qblog.Level, ctx context.Context, tmpl string, args sllm.ArgMap) {
-	(*qblog.Logger)(l).Out(calldepth, level, ctx, sllm.Map(tmpl, args))
+	l.Out(calldepth, level, sllm.Args(tmpl, args...))
 }
 
 func (l *Logger) Map(level qblog.Level, tmpl string, args sllm.ArgMap) {
-	(*qblog.Logger)(l).Out(calldepth, level, nil, sllm.Map(tmpl, args))
+	l.Out(calldepth, level, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Trace(message string) {
+	l.Out(calldepth, qblog.Ltrace, qblog.Msg(message))
+}
+
+func (l *Logger) TraceA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Ltrace, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) TraceM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Ltrace, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Debug(message string) {
+	l.Out(calldepth, qblog.Ldebug, qblog.Msg(message))
+}
+
+func (l *Logger) DebugA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Ldebug, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) DebugM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Ldebug, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Info(message string) {
+	l.Out(calldepth, qblog.Linfo, qblog.Msg(message))
+}
+
+func (l *Logger) InfoA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Linfo, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) InfoM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Linfo, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Warn(message string) {
+	l.Out(calldepth, qblog.Lwarn, qblog.Msg(message))
+}
+
+func (l *Logger) WarnA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Lwarn, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) WarnM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Lwarn, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Error(message string) {
+	l.Out(calldepth, qblog.Lerror, qblog.Msg(message))
+}
+
+func (l *Logger) ErrorA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Lerror, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) ErrorM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Lerror, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Panic(message string) {
+	l.Out(calldepth, qblog.Lpanic, qblog.Msg(message))
+}
+
+func (l *Logger) PanicA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Lpanic, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) PanicM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Lpanic, sllm.Map(tmpl, args))
+}
+
+func (l *Logger) Fatal(message string) {
+	l.Out(calldepth, qblog.Lfatal, qblog.Msg(message))
+}
+
+func (l *Logger) FatalA(tmpl string, args ...interface{}) {
+	l.Out(calldepth, qblog.Lfatal, sllm.Args(tmpl, args...))
+}
+
+func (l *Logger) FatalM(tmpl string, args sllm.ArgMap) {
+	l.Out(calldepth, qblog.Lfatal, sllm.Map(tmpl, args))
 }
--- qbsllm_test.go
+++ qbsllm_test.go
@@ -1,13 +1,35 @@
 package qbsllm
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"os"
+	"time"
 
 	"git.fractalqb.de/fractalqb/qblog"
 )
 
-func ExampleOldNewApi() {
-	l := New(qblog.Linfo, "qbsllm", os.Stdout, qblog.BasicFormat)
-	l.Args(qblog.Linfo, "just a `adj` message", "fancy")
+func testFormat(
+	buf *bytes.Buffer,
+	now time.Time,
+	lvl qblog.Level,
+	title, file string,
+	line int,
+	ctx qblog.Context,
+	message io.WriterTo,
+) {
+	fmt.Fprintf(buf, "%s %s %s:%d <%s>",
+		title,
+		qblog.LevelName(lvl),
+		file, line,
+		message)
+}
+
+func ExampleNewApi() {
+	l := New(qblog.Linfo, "qbsllm", os.Stdout, testFormat)
+	l.SetFlags(qblog.FsrcLoc)
+	l.InfoA("just a `adj` message", "fancy")
 	// Output:
+	// qbsllm INFO qbsllm_test.go:32 <just a `adj:fancy` message>
 }
