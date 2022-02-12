fe2dfe07d7d48e58b5e315934905bd569fb53060
Marcus <marcus@fractalqb.de>
2022-02-12 15:25:12 +0100

    Add log methods to the log subpackage

--- log/log.go
+++ log/log.go
@@ -21,16 +21,26 @@ func title() string {
 	return res
 }
 
-func Infos(msg string) { root.Infos(msg) }
+func Debugs(msg string)                       { root.Debugs(msg) }
+func Debugf(fmt string, args ...interface{})  { root.Debugf(fmt, args...) }
+func Debuga(tmpl string, args ...interface{}) { root.Debuga(tmpl, args...) }
 
-func Infof(fmt string, args ...interface{}) { root.Infof(fmt, args...) }
-
+func Infos(msg string)                       { root.Infos(msg) }
+func Infof(fmt string, args ...interface{})  { root.Infof(fmt, args...) }
 func Infoa(tmpl string, args ...interface{}) { root.Infoa(tmpl, args...) }
 
+func Warns(msg string)                       { root.Warns(msg) }
+func Warnf(fmt string, args ...interface{})  { root.Warnf(fmt, args...) }
+func Warna(tmpl string, args ...interface{}) { root.Warna(tmpl, args...) }
+
+func Errors(msg string)                       { root.Errors(msg) }
+func Errore(err error)                        { root.Errore(err) }
 func Errora(tmpl string, args ...interface{}) { root.Errora(tmpl, args...) }
 
+func Panica(tmpl string, args ...interface{}) { root.Panica(tmpl, args...) }
+func Panice(err error)                        { root.Panice(err) }
+func Panics(msg string)                       { root.Panics(msg) }
+
 func Fatala(tmpl string, args ...interface{}) { root.Fatala(tmpl, args...) }
-
-func Fatale(err error) { root.Fatale(err) }
-
-func Fatals(msg string) { root.Fatals(msg) }
+func Fatale(err error)                        { root.Fatale(err) }
+func Fatals(msg string)                       { root.Fatals(msg) }
